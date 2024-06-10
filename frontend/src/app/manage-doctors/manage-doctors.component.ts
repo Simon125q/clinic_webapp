72910d99431e12749f59687f766d47bc3e1dd10e
Simon125q <sz.omiecinski@gmail.com>
2024-06-10 19:02:05 +0200

    added adding new doc & patients, filtering and sorting

---
+++ frontend/src/app/manage-doctors/manage-doctors.component.ts
@@ -0,0 +1,252 @@
+import {Component, OnInit} from '@angular/core';
+import {Doctor} from "../models/doctor.model";
+import {DoctorService} from "../services/doctor.service";
+import {devOnlyGuardedExpression} from "@angular/compiler";
+import {RouterLink} from "@angular/router";
+import {NgForOf, NgIf} from "@angular/common";
+import {of} from "rxjs";
+import {FormsModule} from "@angular/forms";
+
+@Component({
+  selector: 'app-manage-doctors',
+  standalone: true,
+  imports: [
+    RouterLink,
+    NgForOf,
+    NgIf,
+    FormsModule
+  ],
+  templateUrl: './manage-doctors.component.html',
+  styleUrl: './manage-doctors.component.css'
+})
+export class ManageDoctorsComponent implements OnInit {
+  addingNew: boolean = false;
+  searchPhrase?: string;
+  doctorList?: Doctor[];
+  filteredDoctorList?: Doctor[];
+  doctor?: Doctor;
+  displayDoctor?: Doctor;
+  selectedDoctor?: Doctor;
+
+  constructor(private doctorService: DoctorService) {
+  }
+
+  ngOnInit() {
+    this.getDoctors();
+  }
+
+  sort(key: string): void {
+    if (this.filteredDoctorList == undefined) {
+      this.filteredDoctorList = this.doctorList;
+    }
+    if (this.filteredDoctorList == undefined) {
+      return;
+    }
+    switch (key) {
+      case "id":
+        this.filteredDoctorList = this.filteredDoctorList.sort((a, b) => a.id && b.id ? a.id - b.id: 0);
+        break;
+      case "firstname":
+        this.filteredDoctorList = this.filteredDoctorList.sort((a, b) =>
+        a.firstName.localeCompare(b.firstName, undefined, {sensitivity: "base"}));
+        break;
+      case "lastname":
+        this.filteredDoctorList = this.filteredDoctorList.sort((a, b) =>
+          a.lastName.localeCompare(b.lastName, undefined, {sensitivity: "base"}));
+        break;
+      case "email":
+        this.filteredDoctorList = this.filteredDoctorList.sort((a, b) =>
+          a.email.localeCompare(b.email, undefined, {sensitivity: "base"}));
+        break;
+      case "telephone":
+        this.filteredDoctorList = this.filteredDoctorList.sort((a, b) =>
+          a.telephone.localeCompare(b.telephone, undefined, {sensitivity: "base"}));
+        break;
+      case "specialization":
+        this.filteredDoctorList = this.filteredDoctorList.sort((a, b) =>
+          a.specialization.localeCompare(b.specialization, undefined, {sensitivity: "base"}));
+        break;
+    }
+  }
+
+  search(): void {
+    this.filteredDoctorList = [];
+    if (this.doctorList == undefined) {
+      return;
+    }
+    if (this.searchPhrase == undefined) {
+      this.filteredDoctorList = this.doctorList;
+      return;
+    }
+    for (let doc of this.doctorList) {
+      if (doc.firstName.toLowerCase().includes(this.searchPhrase.trim().toLowerCase())) {
+         this.filteredDoctorList.push(doc);
+      }
+      else if (doc.lastName.toLowerCase().includes(this.searchPhrase.trim().toLowerCase())) {
+        this.filteredDoctorList.push(doc)
+      }
+      else if (doc.email.toLowerCase().includes(this.searchPhrase.trim().toLowerCase())) {
+        this.filteredDoctorList.push(doc)
+      }
+      else if (doc.telephone.toLowerCase().includes(this.searchPhrase.trim().toLowerCase())) {
+        this.filteredDoctorList.push(doc)
+      }
+      else if (doc.specialization.toLowerCase().includes(this.searchPhrase.trim().toLowerCase())) {
+        this.filteredDoctorList.push(doc)
+      }
+      else if (doc.description.toLowerCase().includes(this.searchPhrase.trim().toLowerCase())) {
+        this.filteredDoctorList.push(doc)
+      }
+    }
+  }
+
+  addDoctor(): void {
+    this.addingNew = true;
+    this.hideDetail();
+  }
+
+  displayDetail(doctor: Doctor): void {
+    this.addingNew = false;
+    this.displayDoctor = doctor;
+    this.selectedDoctor = doctor;
+  }
+
+  deleteSelected(): void {
+    if (this.displayDoctor != undefined){
+      this.delete(this.displayDoctor);
+    }
+    this.hideDetail();
+  }
+
+  updateSelected(): void {
+    if (this.displayDoctor != undefined && this.selectedDoctor != undefined) {
+      this.update(this.displayDoctor.firstName, this.displayDoctor.lastName, this.displayDoctor.email,
+        this.displayDoctor.telephone, this.displayDoctor.specialization, this.displayDoctor.description, this.selectedDoctor);
+    }
+    this.hideDetail();
+  }
+
+  hideDetail(): void {
+    this.displayDoctor = undefined;
+    this.selectedDoctor = undefined;
+  }
+
+  getDoctors(): void {
+    this.doctorService.getDoctors()
+      .subscribe(doctorList => this.doctorList = doctorList);
+    if (this.doctorList != undefined) {
+      for (let doc of this.doctorList) {
+        this.filteredDoctorList?.push(doc);
+      }
+    }
+    console.log("doctors");
+    console.log(this.doctorList);
+  }
+
+  add(firstName: string, lastName: string, email: string,
+      telephone: string, specialization: string, description: string): void {
+    firstName = firstName.trim();
+    lastName = lastName.trim();
+    email = email.trim();
+    telephone = telephone.trim();
+    specialization = specialization.trim();
+    description = description.trim();
+    this.doctorService.addDoctor({firstName, lastName, email, telephone, specialization, description} as Doctor)
+      .subscribe({
+        next: (doctor: Doctor) => {this.doctorList?.push(doctor)},
+        error: () => {},
+        complete: () => {
+          this.filteredDoctorList = this.doctorList;
+          if (this.doctorList != undefined) {
+            this.doctorService.totalItems.next(this.doctorList.length);
+            console.log(this.doctorList.length);
+          }
+        }
+      });
+  }
+
+  delete(doctor: Doctor): void {
+    this.doctorList = this.doctorList?.filter(c => c !== doctor);
+    this.doctorService.deleteDoctor(doctor).subscribe(() => {
+      this.filteredDoctorList = this.doctorList;
+      if (this.doctorList != undefined) {
+        this.doctorService.totalItems.next(this.doctorList.length);
+        console.log(this.doctorList.length)
+      }
+    })
+  }
+
+  deleteAll(): void {
+    this.doctorService.deleteDoctors().subscribe(() => {
+      this.filteredDoctorList = this.doctorList;
+      if (this.doctorList != undefined) {
+        this.doctorList.length = 0;
+      }
+    });
+  }
+
+  update(firstName: string, lastName: string, email: string, telephone: string,
+         specialization: string, description: string, chosenToUpdateDoctor: Doctor): void {
+    let id = chosenToUpdateDoctor.id;
+    firstName = firstName.trim();
+    lastName = lastName.trim();
+    email = email.trim();
+    telephone = telephone.trim();
+    specialization = specialization.trim();
+    description = description.trim();
+    console.log(id);
+    if (id != undefined) {
+      this.doctorService.updateDoctor({firstName, lastName, email, telephone, specialization, description} as Doctor, id)
+        .subscribe({
+          next: (doctor: Doctor) => {
+            if (this.doctorList != undefined) {
+              let index = this.doctorList?.indexOf(chosenToUpdateDoctor);
+              this.doctorList[index] = doctor;
+            }
+          },
+          error: () => {
+          },
+          complete: () => {
+            this.filteredDoctorList = this.doctorList;
+            if (this.doctorList != undefined) {
+              this.doctorService.totalItems.next(this.doctorList.length);
+              console.log(this.doctorList.length);
+            }
+          }
+        })
+    }
+  }
+  partialUpdate(doctor: Doctor, firstName: string, lastName: string, email: string,
+                telephone: string, specialization: string, description: string): void {
+    firstName = firstName.trim();
+    lastName = lastName.trim();
+    email = email.trim();
+    telephone = telephone.trim();
+    specialization = specialization.trim();
+    description = description.trim();
+    console.log(doctor.id);
+    if (doctor.id != undefined) {
+      this.doctorService.partialUpdate(doctor, firstName, lastName, email, telephone, specialization, description)
+        .subscribe({
+          next: (updatedDoctor: Doctor) => {
+            if (this.doctorList != undefined) {
+              let index = this.doctorList?.indexOf(doctor);
+              this.doctorList[index] = updatedDoctor;
+            }
+          },
+          error: () => {
+          },
+          complete: () => {
+            this.filteredDoctorList = this.doctorList;
+            if (this.doctorList != undefined) {
+              this.doctorService.totalItems.next(this.doctorList.length);
+              console.log(this.doctorList.length);
+            }
+          }
+        })
+    }
+  }
+
+  protected readonly of = of;
+  protected readonly String = String;
+}
---
+++ frontend/src/app/manage-patients/manage-patients.component.ts
@@ -0,0 +1,224 @@
+import {Component, OnInit} from '@angular/core';
+import {Patient} from "../models/patient.model";
+import {PatientService} from "../services/patient.service";
+import {NgForOf, NgIf} from "@angular/common";
+import {RouterLink} from "@angular/router";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+
+@Component({
+  selector: 'app-manage-patients',
+  standalone: true,
+  imports: [
+    NgForOf,
+    RouterLink,
+    NgIf,
+    ReactiveFormsModule,
+    FormsModule
+  ],
+  templateUrl: './manage-patients.component.html',
+  styleUrl: './manage-patients.component.css'
+})
+export class ManagePatientsComponent implements OnInit {
+  addingNew: boolean = false;
+  searchPhrase?: string;
+  patientList?: Patient[];
+  filteredPatientList?: Patient[];
+  patient?: Patient;
+  displayPatient?: Patient;
+  selectedPatient?: Patient;
+  constructor(private patientService: PatientService) {
+  }
+
+  ngOnInit() {
+    this.getPatients();
+  }
+
+  sort(key: string): void {
+    if (this.filteredPatientList == undefined) {
+      this.filteredPatientList = this.patientList;
+    }
+    if (this.filteredPatientList == undefined) {
+      return;
+    }
+    switch (key) {
+      case "id":
+        this.filteredPatientList = this.filteredPatientList.sort((a, b) => a.id && b.id ? a.id - b.id: 0);
+        break;
+      case "firstname":
+        this.filteredPatientList = this.filteredPatientList.sort((a, b) =>
+          a.firstName.localeCompare(b.firstName, undefined, {sensitivity: "base"}));
+        break;
+      case "lastname":
+        this.filteredPatientList = this.filteredPatientList.sort((a, b) =>
+          a.lastName.localeCompare(b.lastName, undefined, {sensitivity: "base"}));
+        break;
+      case "email":
+        this.filteredPatientList = this.filteredPatientList.sort((a, b) =>
+          a.email.localeCompare(b.email, undefined, {sensitivity: "base"}));
+        break;
+      case "telephone":
+        this.filteredPatientList = this.filteredPatientList.sort((a, b) =>
+          a.telephone.localeCompare(b.telephone, undefined, {sensitivity: "base"}));
+        break;
+
+    }
+  }
+
+  search(): void {
+    this.filteredPatientList = [];
+    if (this.patientList == undefined) {
+      return;
+    }
+    if (this.searchPhrase == undefined) {
+      this.filteredPatientList = this.patientList;
+      return;
+    }
+    for (let doc of this.patientList) {
+      if (doc.firstName.toLowerCase().includes(this.searchPhrase.trim().toLowerCase())) {
+        this.filteredPatientList.push(doc);
+      }
+      else if (doc.lastName.toLowerCase().includes(this.searchPhrase.trim().toLowerCase())) {
+        this.filteredPatientList.push(doc)
+      }
+      else if (doc.email.toLowerCase().includes(this.searchPhrase.trim().toLowerCase())) {
+        this.filteredPatientList.push(doc)
+      }
+      else if (doc.telephone.toLowerCase().includes(this.searchPhrase.trim().toLowerCase())) {
+        this.filteredPatientList.push(doc)
+      }
+    }
+  }
+
+  addPatient(): void {
+    this.addingNew = true;
+    this.hideDetail();
+  }
+
+  displayDetail(patient: Patient): void {
+    this.addingNew = false;
+    this.displayPatient = patient;
+    this.selectedPatient = patient;
+  }
+
+  deleteSelected(): void {
+    if (this.displayPatient != undefined){
+      this.delete(this.displayPatient);
+    }
+    this.hideDetail();
+  }
+
+  updateSelected(): void {
+    if (this.displayPatient != undefined && this.selectedPatient != undefined) {
+      this.update(this.displayPatient.firstName, this.displayPatient.lastName, this.displayPatient.email,
+        this.displayPatient.telephone, this.selectedPatient);
+    }
+    this.hideDetail();
+  }
+
+  hideDetail(): void {
+    this.displayPatient = undefined;
+    this.selectedPatient = undefined;
+  }
+
+  getPatients(): void {
+    this.patientService.getPatients()
+      .subscribe(patientList => this.patientList = patientList);
+    console.log("patients");
+    console.log(this.patientList);
+  }
+
+  add(firstName: string, lastName: string, email: string,
+      telephone: string): void {
+    firstName = firstName.trim();
+    lastName = lastName.trim();
+    email = email.trim();
+    telephone = telephone.trim();
+    this.patientService.addPatient({firstName, lastName, email, telephone} as Patient)
+      .subscribe({
+        next: (patient: Patient) => {this.patientList?.push(patient)},
+        error: () => {},
+        complete: () => {
+          if (this.patientList != undefined) {
+            this.patientService.totalItems.next(this.patientList.length);
+            console.log(this.patientList.length)
+          }
+        }
+      });
+  }
+
+  delete(patient: Patient): void {
+    this.patientList = this.patientList?.filter(c => c !== patient);
+    this.patientService.deletePatient(patient).subscribe(() => {
+      if (this.patientList != undefined) {
+        this.patientService.totalItems.next(this.patientList.length);
+        console.log(this.patientList.length)
+      }
+    })
+  }
+
+  deleteAll(): void {
+    this.patientService.deletePatients().subscribe(() => {
+      if (this.patientList != undefined) {
+        this.patientList.length = 0;
+      }
+    });
+  }
+
+  update(firstName: string, lastName: string, email: string, telephone: string, chosenToUpdatePatient: Patient): void {
+    let id = chosenToUpdatePatient.id;
+    firstName = firstName.trim();
+    lastName = lastName.trim();
+    email = email.trim();
+    telephone = telephone.trim();
+
+    console.log(id);
+    if (id != undefined) {
+      this.patientService.updatePatient({firstName, lastName, email, telephone} as Patient, id)
+        .subscribe({
+          next: (patient: Patient) => {
+            if (this.patientList != undefined) {
+              let index = this.patientList?.indexOf(chosenToUpdatePatient);
+              this.patientList[index] = patient;
+            }
+          },
+          error: () => {
+          },
+          complete: () => {
+            if (this.patientList != undefined) {
+              this.patientService.totalItems.next(this.patientList.length);
+              console.log(this.patientList.length);
+            }
+          }
+        })
+    }
+  }
+  partialUpdate(patient: Patient, firstName: string, lastName: string, email: string,
+                telephone: string): void {
+    firstName = firstName.trim();
+    lastName = lastName.trim();
+    email = email.trim();
+    telephone = telephone.trim();
+    console.log(patient.id);
+    if (patient.id != undefined) {
+      this.patientService.partialUpdate(patient, firstName, lastName, email, telephone)
+        .subscribe({
+          next: (updatedPatient: Patient) => {
+            if (this.patientList != undefined) {
+              let index = this.patientList?.indexOf(patient);
+              this.patientList[index] = updatedPatient;
+            }
+          },
+          error: () => {
+          },
+          complete: () => {
+            if (this.patientList != undefined) {
+              this.patientService.totalItems.next(this.patientList.length);
+              console.log(this.patientList.length);
+            }
+          }
+        })
+    }
+  }
+
+}
+
